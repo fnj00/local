97f5be29bccc114ab0908d809078ee4f6217c25b
Joseph Hayhoe <fnj00mail@gmail.com>
2026-04-14 19:00:31 -0400

    Create action.php

---
+++ admin/action.php
@@ -0,0 +1,45 @@
+<?php
+include("../includes/db.php");
+
+//action.php
+$input = filter_input_array(INPUT_POST);
+
+print_r($input);
+
+if($input["action"] === 'edit')
+{
+ $query = "
+ UPDATE events
+ SET title = '".$input["title"]."',
+ date = '".$input["date"]."',
+ collagex = '".$input["collagex"]."',
+ collagey = '".$input["collagey"]."',
+ numcollage = '".$input["numcollage"]."',
+ autoapprove = '".$input["autoapprove"]."'
+ WHERE ID = '".$input["ID"]."'
+ ";
+
+ if (!$mysqli->query($query)) {
+  // Oh no! The query failed.
+  printf("Errormessage: %s\n", $mysqli->error);
+  exit;
+ }
+}
+
+if($input["action"] === 'delete')
+{
+ $query = "
+ DELETE FROM events
+ WHERE ID = '".$input["ID"]."'
+ ";
+
+ if (!$mysqli->query($query)) {
+  // Oh no! The query failed.
+ printf("Errormessage: %s\n", $mysqli->error);
+ exit;
+ }
+}
+
+echo json_encode($input);
+
+?>
